b704e26051d086ca9b7299bc272e0714507e4a40
aubrcart224 <aubrcart224@ed.amdsb.ca>
2021-05-03 13:55:52 -0400

    Add project files.

---
+++ pizzashop/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace pizzashop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numberofLabel = new System.Windows.Forms.Label();
+            this.inputText = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // numberofLabel
+            // 
+            this.numberofLabel.AutoSize = true;
+            this.numberofLabel.Location = new System.Drawing.Point(53, 51);
+            this.numberofLabel.Name = "numberofLabel";
+            this.numberofLabel.Size = new System.Drawing.Size(103, 13);
+            this.numberofLabel.TabIndex = 0;
+            this.numberofLabel.Text = "Number of Toppings";
+            // 
+            // inputText
+            // 
+            this.inputText.Location = new System.Drawing.Point(194, 51);
+            this.inputText.Name = "inputText";
+            this.inputText.Size = new System.Drawing.Size(100, 20);
+            this.inputText.TabIndex = 1;
+            this.inputText.TextChanged += new System.EventHandler(this.toppingText_TextChanged);
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(132, 135);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(97, 47);
+            this.calculateButton.TabIndex = 2;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Location = new System.Drawing.Point(106, 255);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(211, 86);
+            this.outputLabel.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.inputText);
+            this.Controls.Add(this.numberofLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label numberofLabel;
+        private System.Windows.Forms.TextBox inputText;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ pizzashop/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pizzashop
+{
+    public partial class Form1 : Form
+    {
+        //global varibales 
+        
+        double baseprice = 7.00;
+        double toppingprice = 1.25;
+        double toppingnumber; 
+        double total;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {   try
+            {
+                toppingnumber = Convert.ToDouble(inputText.Text);
+                total = toppingprice * toppingnumber + baseprice;
+                outputLabel.Text = $"The price of the pizza with {toppingnumber} toppings is ${total} thank you.";
+            }
+            catch { outputLabel.Text = $"only imnput numbers"; }
+        }
+
+        private void toppingText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
